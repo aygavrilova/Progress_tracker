db1fb3ad21432e9732223760df8f7f7f6242e9aa
Anna Gavrilova <annagavrilovamrs@gmail.com>
2022-08-14 17:35:56 -0400

    packages json

---
+++ server/api/awsApi.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk')
+const config = require('../config.json')
+
+const s3Config = {
+    apiVersion: '2006-03-01',
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey,
+    region: config.region,
+   }
+
+const s3 = new AWS.S3(s3Config)
---
+++ server/repository/profileRepository.js
@@ -0,0 +1,85 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const dbName = "progress_tracker";
+const dbColl = "profiles";
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const getAllProfiles = async()=>{
+
+    const client = new MongoClient(MONGO_URI, options);
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        const cursor = await db.collection(dbColl).find()
+        const users = await cursor.toArray();
+
+        const result = {ok:true, users:users}
+        return result;
+    }
+    catch(err){
+        console.log(err);
+        return {
+            ok:false,
+            error:err
+        };
+    }finally{
+        await client.close();
+    }
+};
+
+const getProfile = async(userId, projection) =>{
+    const client = new MongoClient(MONGO_URI, options);
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        const query = {user_id:userId}
+
+        if(projection){
+            const options = {projection : projection}
+        }
+
+        const profile = await db.collection(dbColl).findOne({user_id:userId})
+        
+        const result = {ok:true, profile:profile}
+        return result;
+    }
+    catch(err){
+        console.log(err);
+        return {
+            ok:false,
+            error:err
+        };
+    }finally{
+        await client.close();
+    }
+}
+
+
+const updateProfile = async(userId, updateObject)=>{
+    const client = new MongoClient(MONGO_URI, options);
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        await db.collection(dbColl).updateOne({user_id  : userId}, {$set: updateObject})
+        const result = {
+            ok:true, 
+        }
+        return result;
+    }
+    catch(err){
+        console.log(err);
+        return {
+            ok:false,
+            error:err
+        };
+    }finally{
+        await client.close();
+    }
+}
+
+module.exports = { getAllProfiles,getProfile,updateProfile };
