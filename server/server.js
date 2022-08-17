3ff5cc66b03a745552aaac3dceb0b86279966a29
Anna Gavrilova <annagavrilovamrs@gmail.com>
2022-08-17 11:27:29 -0400

    final server changes

--- server/server.js
+++ server/server.js
@@ -3,37 +3,43 @@ const morgan = require('morgan')
 const PORT = 8000
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+
+const multer = require('multer')
+const upload = multer({dest: './uploads'})
 require("dotenv").config();
 const {
-    getUsersHandler,
-    getProfileHandler,
+    getProfileGoalsHandler,
     getGoalsHandler,
     getGoalHandler,
     getCurrProfileHandler,
     createGoalHandler,
     patchCurrProfileHandler,
     patchProfileEmailsPrefsHandler,
-    patchProfileUserInfoHandler
+    patchProfileUserInfoHandler,
+    patchGoalStepHandler,
+    updateGoalHandler,
+    uploadImageHandler,
+    getImageHandler
 } = require('./handlers.js')
 
 const { auth, requiresAuth } = require('express-openid-connect');
 const checkJwt = jwt({
     secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: "https://dev-jkinoppb.us.auth0.com/.well-known/jwks.json",
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://dev-jkinoppb.us.auth0.com/.well-known/jwks.json",
     }),
-  
+
     audience: "https://dev-jkinoppb.us.auth0.com/api/v2/",
     issuer: "https://dev-jkinoppb.us.auth0.com/",
     algorithms: ["RS256"],
-  });
+});
 
 
-  const extractToken = (req,res,next)=>{
+const extractToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
-    if(typeof bearerHeader === "undefined"){
+    if (typeof bearerHeader === "undefined") {
         return res.sendStatus(403);
     }
 
@@ -42,7 +48,18 @@ const checkJwt = jwt({
     req.token = token;
 
     next();
-  }
+}
+
+const extractTokenIfAny = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(' ');
+        const token = bearer[1];
+        req.token = token;
+    }
+
+    next();
+}
 
 express()
 
@@ -68,21 +85,20 @@ express()
         res.status(200).json({ status: 200, message: "Hello!" })
     })
 
-    .get("/v1/api/profiles",getUsersHandler)
-    .get("/v1/api/profiles/:id", getProfileHandler)
-
-    // auth required
-    .get("/v1/api/profile",extractToken,checkJwt,getCurrProfileHandler)
+    .get("/v1/api/profile", extractToken, checkJwt, getCurrProfileHandler)
     .patch("/v1/api/profile", extractToken, checkJwt, patchCurrProfileHandler)
     .patch("/v1/api/profile/emailPrefs", extractToken, checkJwt, patchProfileEmailsPrefsHandler)
     .patch("/v1/api/profile/userInfo", extractToken, checkJwt, patchProfileUserInfoHandler)
+    .get("/v1/api/profile/:id/goals", getProfileGoalsHandler)
 
-    .get("/v1/api/goals", getGoalsHandler)
-    .get("/v1/api/goals/:id", getGoalHandler)
-    
-    // auth required
-    .post("/v1/api/goals",extractToken, checkJwt, createGoalHandler)
+    .get("/v1/api/goals",getGoalsHandler )
+    .get("/v1/api/goals/:id", extractTokenIfAny, getGoalHandler)
+    .post("/v1/api/goals", extractToken, checkJwt, createGoalHandler)
+    .put("/v1/api/goals", extractToken, checkJwt,updateGoalHandler )
+    .patch("/v1/api/goals/:id/steps/:stepId", extractToken, checkJwt, patchGoalStepHandler)
 
+    .get("/v1/api/images/:key", getImageHandler)
+    .post("/v1/api/images",extractToken, checkJwt, upload.single('image'), uploadImageHandler)
 
     .listen(PORT, () => {
         console.log(`Example app listening on port ${PORT}`)
